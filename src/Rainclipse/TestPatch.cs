d8e7754a587c04163be6ca36d2b9f6d77c7a8372
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2025-02-04 00:45:51 +1000

    Update to v2

--- src/Rainclipse/TestPatch.cs
+++ src/Rainclipse/TestPatch.cs
@@ -1,5 +1,7 @@
 ﻿using Hypercube.Graphics.Patching;
 using Hypercube.Graphics.Rendering.Context;
+using Hypercube.Mathematics;
+using Hypercube.Mathematics.Shapes;
 
 namespace Rainclipse;
 
@@ -7,6 +9,6 @@ public sealed class TestPatch : Patch
 {
     public override void Draw(IRenderContext renderer)
     {
-
+        renderer.DrawRectangle(new Box2(0, 1, 1, 0), Color.Blue);
     }
 }
