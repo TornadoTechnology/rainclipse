ae475b00e126bc68db905d99fe5b84e36400c046
Tornado Tech <54727692+Tornado-Technology@users.noreply.github.com>
2025-03-10 22:50:46 +1000

    feat: Engine update

---
+++ src/Rainclipse/TestComponent.cs
@@ -0,0 +1,7 @@
+﻿using Hypercube.Core.Ecs.Attributes;
+using Hypercube.Core.Ecs.Components;
+
+namespace Rainclipse;
+
+[RegisterComponent]
+public sealed class TestComponent : Component;
---
+++ src/Rainclipse/TestSystem.cs
@@ -0,0 +1,16 @@
+﻿using Hypercube.Core.Ecs;
+using Hypercube.Core.Ecs.Attributes;
+using Hypercube.Core.Ecs.Events;
+using Hypercube.Core.Ecs.Systems;
+
+namespace Rainclipse;
+
+[RegisterEntitySystem]
+public sealed class TestSystem : EntitySystem
+{
+    [Subscribe]
+    public void OnAdded(Entity entity, TestComponent component, ref AddedEvent _)
+    {
+        Logger.Debug($"{nameof(TestComponent)} added to {entity}");
+    }
+}
