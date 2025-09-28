ce5828e182e30ec6482bb657cb624a860437f9c7
Tornado Tech <54727692+TornadoTechnology@users.noreply.github.com>
2025-09-29 01:42:42 +1000

    feat: Engine update

--- src/Rainclipse/TestPatch.cs
+++ src/Rainclipse/TestPatch.cs
@@ -24,7 +24,7 @@ public sealed class TestPatch : Patch, IPostInject
     
     public void OnPostInject()
     {
-        _font = _resource.Load<Font>("/fonts/OpenSans.ttf", [("size", 16)]);
+        _font = _resource.Load<Font>("/fonts/OpenSans.ttf", [("size", 24)]);
         _model = _resource.Load<Model>("/models/teapot.obj");
         _texture = _resource.Load<Texture>("/textures/default.png");
     }
@@ -34,8 +34,8 @@ public sealed class TestPatch : Patch, IPostInject
         _segments += 0.01f;
         _rotation = _rotation.WithX(_rotation.X + 0.01f);
         
-        renderer.DrawTexture(_font.Texture, Vector2.Zero, Angle.Zero, Vector2.One, Color.White);
-        renderer.DrawText($"{_render.BatchCount}\n{_render.VerticesCount}\n{_render.Fps}", _font, new Vector2(-300, 200), Color.White);
+        // renderer.DrawTexture(_font.Texture, Vector2.Zero, Angle.Zero, Vector2.One, Color.White);
+        renderer.DrawText($"{_render.BatchCount}|{_render.VerticesCount}|{_render.Fps}|異体", _font, new Vector2(-300, 200), Color.White);
         renderer.DrawCircle(new Vector2(100, 0), 50, Color.Beige, (int) _segments);
         renderer.DrawModel(_model, Vector3.Zero, Quaternion.FromEuler(_rotation), Vector3.One * 30, Color.White, _texture);
     }
